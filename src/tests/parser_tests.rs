0f0169fb9d54025086818826ae38a0b51baedd4e
Jonas Colve <jonas.colve@outlook.de>
2019-12-08 14:05:03 +0100

    Added grammar tests for expression and function_def

--- src/tests/parser_tests.rs
+++ src/tests/parser_tests.rs
@@ -58,3 +58,91 @@ fn function_dcl() {
             ]) ]
     }
 }
+
+#[test]
+fn function_def() {
+    pest::parses_to! {
+        parser:SplispParser,
+        input : "func ( x : X, y:Y ) -> Z\n\t= (g(x),y)",
+        rule : Rule::function_def,
+        tokens: [
+            function_def(0, 36, [
+                identifier( 0, 4, []),
+                function_sig( 5, 24, [
+                    parameter_list(5, 19, [
+                        identifier( 7, 8, [] ),
+                        identifier( 11, 12, [] ),
+                        identifier( 14, 15, [] ),
+                        identifier( 16, 17, [] ),
+                    ]),
+                    identifier(23, 24, [])
+                ]),
+                list( 28, 36, [
+                    function_call( 29, 33, [
+                        identifier( 29, 30 ),
+                        list( 30, 33, [
+                        identifier( 31, 32, [])
+                        ] )
+                    ]),
+                    identifier( 34, 35, [] )
+                ] )
+            ]) ]
+    }
+}
+
+#[test]
+fn expression() {
+    let text = "let x:X = 0;\nlet y:= ();\nx";
+    println!("{}",text);
+    pest::parses_to! {
+        parser:SplispParser,
+        input : text,
+        rule : Rule::expression,
+        tokens: [
+            let_var(0, 12, [
+                identifier( 4, 5, [] ),
+                identifier( 6, 7, [] ),
+                integer( 10, 11, [] )
+            ]),
+            let_var(13, 24, [
+                identifier( 17, 18, [] ),
+                list( 21, 23, [] )
+            ]),
+            identifier(25,26,[] )
+        ]
+    }
+    let text = "( x, 0, () )";
+    println!("{}",text);
+    pest::parses_to! {
+        parser:SplispParser,
+        input : text,
+        rule : Rule::expression,
+        tokens: [
+            list( 0, 12, [
+                  identifier(2,3),
+                  integer(5,6),
+                  list(8,10)
+            ] )
+        ]
+    }
+    let text = "0.1";
+    println!("{}",text);
+    pest::parses_to! {
+        parser:SplispParser,
+        input : text,
+        rule : Rule::expression,
+        tokens: [
+            float( 0, 3, [] )
+        ]
+    }
+    let text = "\"Hello \\\"World\\\"\"";
+    println!("{}",text);
+    pest::parses_to! {
+        parser:SplispParser,
+        input : text,
+        rule : Rule::expression,
+        tokens: [
+            string( 0, 17, [] )
+        ]
+    }
+}
