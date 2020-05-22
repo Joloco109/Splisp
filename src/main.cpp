92a09ebd3d68a4b53290d511688e7fe8e91de53b
Samuel Mahler <mahlersand@gmail.com>
2020-05-22 18:13:30 +0200

    Let's try this
    Signed-off-by: Samuel Mahler <mahlersand@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <QCommandLineParser>
+
+#include <src/parser.h>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("splispc");
+    QCoreApplication::setApplicationVersion("0.0.1");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(
+                "\nsplisp reference compiler\n\n"
+                "By Samuel Mahler <mahlersand@gmail.com>\n"
+                "   Jonas Colve <jonas.colve@outlook.de>\n"
+                "   Philipp Rosendahl <philipp.rosendahl@gmx.de>"
+    );
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addPositionalArgument("file", "file");
+
+    parser.process(app);
+
+
+
+    return app.exec();
+}
