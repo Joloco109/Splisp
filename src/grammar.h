b3b9f976f9088d96e06da4bf8010c12853a22fb5
Samuel Mahler <mahlersand@gmail.com>
2020-05-25 20:09:01 +0200

    Remove unneeded references to PEGTL.
    Signed-off-by: Samuel Mahler <mahlersand@gmail.com>

--- src/grammar.h
+++ src/grammar.h
@@ -3,52 +3,49 @@
 
 #include <boost/spirit/home/x3.hpp>
 
-/*namespace pegtl = tao::pegtl;
-
 namespace splgrammar {
-    struct ignored : pegtl::sor<pegtl::space>{};
 
-    struct str_func : pegtl::string<'f','u','n','c'>{};
-    struct str_type : pegtl::string<'t','y','p','e'>{};
-    struct str_trait : pegtl::string<'t','r','a','i','t'>{};
-    struct str_thorp : pegtl::string<'#'>{};
+namespace x3 = boost::spirit::x3;
 
-    template<typename T>
-    struct inst : pegtl::pad<T, ignored>{};
+auto const lex = x3::lexeme;
+using x3::char_;
+using x3::lit;
 
-    struct reserved :
-            pegtl::sor<
-            str_func,
-            str_type,
-            str_trait
-            >{};
+//We ain't gonna have an octotorpe, sorry
+auto const spl_escape = char_('#');
+auto const spl_ellipsis = lit("...");
 
-    // (A-Za-z_) ~ (UTF8-ICU-ALPHA | c-style-nonfirst-id)*
-    struct identifier :
-            pegtl::seq<
-                pegtl::ascii::identifier_first,
-                pegtl::star< pegtl::sor<
-                    pegtl::utf8::icu::alphabetic,
-                    pegtl::identifier_other
-                >>
-            >{};
+auto const sl_comment_terminal = (x3::eoi | x3::eol);
+auto const sl_comment = spl_escape >> spl_escape >> *( char_ - sl_comment_terminal) >> sl_comment_terminal;
+auto const ml_comment_terminal = lit("*#");
+auto const ml_comment = lit("#*") >> *(char_ - ml_comment_terminal) >> ml_comment_terminal;
 
-    // (*|:*|*:*)
-    struct atomic_pattern{};
-};*/
+//Build a parser that parses what we want to skip
+auto const skip = (x3::space | sl_comment | ml_comment);
 
-namespace splgrammar {
-    namespace x3 = boost::spirit::x3;
+auto const identifier = x3::alpha >> *x3::alnum;
 
-    auto spl_escape = x3::char_('#');
+//literals
+auto const lit_string = lex['"' >> *(char_ - ('"' - lit("\\\""))) >> '"'];
+auto const lit_any = lex[(lit_string
+                          | x3::long_
+                          | x3::double_) >> ('_' >> +x3::alnum)];
 
-    auto sl_comment_terminal = (x3::eoi | x3::eol);
-    auto sl_comment = spl_escape >> spl_escape >> *( x3::char_ - sl_comment_terminal) >> sl_comment_terminal;
-    auto ml_comment_terminal = x3::lit("*#");
-    auto ml_comment = x3::lit("#*") >> *(x3::char_ - ml_comment_terminal) >> ml_comment_terminal;
+auto const atomic_pattern_expandable = identifier >> -spl_ellipsis;
+auto const atomic_pattern_nonexpandable = identifier;
 
-    //Build a parser that parses what we want to skip
-    auto skip = (x3::space | sl_comment | ml_comment);
-}
+auto const inner_pattern = (atomic_pattern_nonexpandable
+                            | ':' >> atomic_pattern_expandable
+                            | atomic_pattern_nonexpandable >> ':' >> atomic_pattern_expandable);
+
+auto const pattern = '(' >> inner_pattern >> ')';
+auto const metapattern = '<' >> inner_pattern >> '>';
+
+
+
+
+
+
+} // namespace splgrammar
 
 #endif // GRAMMAR_H
--- src/parser.h
+++ src/parser.h
@@ -1,9 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include <tao/pegtl.hpp>
-#include <tao/pegtl/contrib/icu/utf8.hpp>
-
 #include "grammar.h"
 
 class Parser
